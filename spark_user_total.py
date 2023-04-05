5b495cf1e781198421b676eba2d1ddca4fd2fd73
bharath6969 <127783908+bharath6969@users.noreply.github.com>
2023-04-04 21:53:23 -0700

    Create spark_user_total.py

---
+++ spark_user_total.py
@@ -0,0 +1,13 @@
+from pyspark.sql.functions import col, coalesce, count, current_timestamp,lit
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.config("spark.sql.warehouse.dir","/user/hive/warehouse").enableHiveSupport().getOrCreate()
+df = spark.table("project.user_data_hive")
+df1 = df.select(current_timestamp().alias("time_ran"), count("*").alias("total_users"))
+df2 = spark.table("project.user_total").select(col("time_ran"), col("total_users"))
+final = df1.join(df2, df1["time_ran"] > df2["time_ran"], how="left") \
+           .select(df1["time_ran"], df1["total_users"], (df1["total_users"] - coalesce(df2["total_users"], lit(0))).alias("users_added")) \
+           .orderBy(df1["time_ran"])
+
+#format("hive") because by default it would be "parquet"
+final.write.mode("append").format("hive").saveAsTable("project.user_total")
