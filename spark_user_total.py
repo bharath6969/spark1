10a433e7028a8fcbd5db6904e0afd15a09402179
bharath6969 <127783908+bharath6969@users.noreply.github.com>
2023-04-04 21:56:28 -0700

    Update spark_user_total.py

--- spark_user_total.py
+++ spark_user_total.py
@@ -5,7 +5,7 @@ spark = SparkSession.builder.config("spark.sql.warehouse.dir","/user/hive/wareho
 df = spark.table("project.user_data_hive")
 df1 = df.select(current_timestamp().alias("time_ran"), count("*").alias("total_users"))
 df2 = spark.table("project.user_total").select(col("time_ran"), col("total_users"))
-final = df1.join(df2, df1["time_ran"] > df2["time_ran"], how="left") \
+final = df1.join(df2, df1["time_ran"] > df2["time_ran"], "left") \
            .select(df1["time_ran"], df1["total_users"], (df1["total_users"] - coalesce(df2["total_users"], lit(0))).alias("users_added")) \
            .orderBy(df1["time_ran"])
 
